0b0b0b02a97823e2bcbf85b073e4337ec1c45590
takato256 <c1139952@planet.kanazawa-it.ac.jp>
2022-07-26 15:19:21 +0900

    first commit

---
+++ ProcessStart/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace ProcessStart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //下記のPythonスクリプトへのファイルパスを記述する
+            string myPythonApp = "test.py";
+
+
+            var myProcess = new Process
+            {
+                StartInfo = new ProcessStartInfo("python.exe")
+                {
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    Arguments = myPythonApp
+                }
+            };
+
+            myProcess.Start();
+            StreamReader myStreamReader = myProcess.StandardOutput;
+            while (true)
+            {
+                string myString = myStreamReader.ReadLine();
+                Console.WriteLine(myString);
+            }
+            myProcess.Close();
+
+        }
+    }
+}
